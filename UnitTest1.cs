99c9f59a1a51c40de12ee4397c835018e5b65bf6
John Hershberg <johnher@gmail.com>
2023-04-05 11:45:07 +0200

    Adding FluentDocker and creating the image

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,7 +1,17 @@
+using Ductus.FluentDocker.Builders;
+
 namespace Step1;
 
 public class UnitTest1
 {
+    public UnitTest1()
+    {
+        var Docker = new Builder()
+            .DefineImage("ganache-testing")
+            .From("trufflesuite/ganache:v7.7.7")
+            .Builder().Build();
+    }
+    
     [Fact]
     public void Test1()
     {
