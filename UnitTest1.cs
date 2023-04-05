59118b3073d003c4e630df8e96b22505a0e6e25d
John Hershberg <johnher@gmail.com>
2023-04-05 11:45:24 +0200

    Reusing the image if it already exists

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -7,7 +7,7 @@ public class UnitTest1
     public UnitTest1()
     {
         var Docker = new Builder()
-            .DefineImage("ganache-testing")
+            .DefineImage("ganache-testing").ReuseIfAlreadyExists()
             .From("trufflesuite/ganache:v7.7.7")
             .Builder().Build();
     }
