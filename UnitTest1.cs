c880e8d2c4739dc5e7490ebab68000733bc13a19
John Hershberg <johnher@gmail.com>
2023-04-05 12:25:48 +0200

    Exposing Port and setting up Web3

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,12 +1,15 @@
 using Ductus.FluentDocker.Builders;
 using Ductus.FluentDocker.Services;
+using Ductus.FluentDocker.Services.Extensions;
+using Nethereum.Web3;
 using Nethereum.Web3.Accounts;
 
 namespace Step1;
 
-public class UnitTest1: IDisposable
+public class UnitTest1: IAsyncLifetime
 {
     private readonly IContainerService Docker;
+    private readonly Web3 Web3;
     
     public UnitTest1()
     {
@@ -17,21 +20,32 @@ public class UnitTest1: IDisposable
         Docker = new Builder()
             .DefineImage(ImageName).ReuseIfAlreadyExists()
             .From("trufflesuite/ganache:v7.7.7")
+            .ExposePorts(8545)
             .Builder()
 
             // Container definition
             .UseContainer()
             .WithName(Guid.NewGuid().ToString("D").ToLower())
             .UseImage(ImageName)
+            .ExposePort(8545)
             .Command($"""--wallet.accounts "{Account.PrivateKey},1000000000000000000000" """)
 
             .Build()
             .Start();
+
+        var ExposedPort = Docker.ToHostExposedEndpoint("8545/tcp").Port;
+        Web3 = new Web3(Account, $"http://localhost:{ExposedPort}");
     }
-    
-    public void Dispose()
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
     {
         Docker.Dispose();
+        return Task.CompletedTask;
     }
     
     [Fact]
